4f0902da0fa10bacfaba46822b24cfeffaf8ea85
Cole Helbling <cole.helbling@determinate.systems>
2022-12-06 13:24:46 -0800

    Mucking with isert_dkms
    Bro the source doesn't even compile because of a typo.
    
    But also it appears to require old kernel stuff, just like iser_dkms
    (who knew).
    
        /build/src/usr/src/isert-5.8/ib_isert.c: In function 'isert_handle_scsi_cmd':
        /build/src/usr/src/isert-5.8/ib_isert.c:1136:17: error: implicit declaration of function 'iscsit_set_unsoliticed_dataout'; did you mean 'iscsit_set_unsolicited_dataout'? [8;;https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wimplicit-function-declaration-Werror=implicit-function-declaration8;;]
         1136 |                 iscsit_set_unsoliticed_dataout(cmd);
              |                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
              |                 iscsit_set_unsolicited_dataout

---
+++ nix/doca-runtime/isert_dkms.nix
@@ -0,0 +1,58 @@
+{ pkgs
+, stdenv
+, self
+, kernel
+}:
+stdenv.mkDerivation rec {
+  pname = "isert-dkms";
+  version = "5.8-OFED.5.8.0.4.8.1";
+
+  src = ../../nv + "/${pname}_${version}_all.deb";
+
+  nativeBuildInputs = with pkgs;
+    [
+      autoPatchelfHook
+      dpkg
+    ] ++ kernel.moduleBuildDependencies;
+
+  # FIXME: see if it loads in a vm test or something; this may be necessary
+  # hardeningDisable = [ "pic" ];
+
+  buildFlags = [
+    # "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "K_BUILD=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "K_SRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/source"
+  ];
+
+  unpackPhase = ''
+    runHook preUnpack
+
+    dpkg-deb -x $src ./src
+    ls -al src/usr/src
+    pushd src/usr/src/isert-*
+
+    runHook postUnpack
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+
+    for module in $(find -iname '*.ko'); do
+      install -D "$module" $out/lib/modules/${kernel.modDirVersion}/extra/"$(basename "$module")"
+    done
+
+    popd
+    mv src/* $out
+
+    tester() {
+      dir="$1"
+      test -d "$1" && (cp -r "$1"/* "$1"/.. && rm -r "$1") || (return 0)
+    }
+
+    tester $out/usr
+
+    runHook postInstall
+  '';
+}
